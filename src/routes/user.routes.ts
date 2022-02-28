d83b608302d465e5d1ef4b607cd54e0aa1a125d6
Rafael Angonese <rafael.angonese09@gmail.com>
2022-02-28 20:25:14 -0300

    :sparkles: feat: created update user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import { CreateUserController } from '../controllers/users/CreateUserController'
 import { ListUserController } from '../controllers/users/ListUserController'
 import { ShowUserController } from '../controllers/users/ShowUserController'
+import { UpdateUserController } from '../controllers/users/UpdateUserController'
 
 const createUserController = new CreateUserController()
 const listUserController = new ListUserController()
 const showUserController = new ShowUserController()
+const updateUserController = new UpdateUserController()
 
 
 const router = Router()
@@ -13,5 +15,6 @@ const router = Router()
 router.get('/', listUserController.handle)
 router.post('/', createUserController.handle)
 router.get('/:id', showUserController.handle)
+router.put('/:id', updateUserController.handle)
 
 export default router
