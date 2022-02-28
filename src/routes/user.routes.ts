7f30e5f701314be7c631ef38b60c8bce2548cd22
Rafael Angonese <rafael.angonese09@gmail.com>
2022-02-28 19:58:26 -0300

    :sparkles: feat: created show user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express'
 import { CreateUserController } from '../controllers/users/CreateUserController'
 import { ListUserController } from '../controllers/users/ListUserController'
+import { ShowUserController } from '../controllers/users/ShowUserController'
 
 const createUserController = new CreateUserController()
 const listUserController = new ListUserController()
+const showUserController = new ShowUserController()
 
 
 const router = Router()
 
 router.get('/', listUserController.handle)
 router.post('/', createUserController.handle)
+router.get('/:id', showUserController.handle)
 
 export default router
