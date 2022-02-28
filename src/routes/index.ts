42f44a216f7213ac0e3f9c96c8f42aabc4310ab5
Rafael Angonese <rafael.angonese09@gmail.com>
2022-02-28 00:34:01 -0300

    :sparkles: feat: created user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 
+import userRoutes from './user.routes'
+
 const router = Router()
 
 router.get('/', (_request, response) => {
     return response.json({ hello: 'Welcome to Dentro De Um Critoen!' })
 })
+
+router.use('/users', userRoutes)
 
 export default router
