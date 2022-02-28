00ea98d531fe9d60b7a98e17339f007d92828633
Rafael Angonese <rafael.angonese09@gmail.com>
2022-02-28 19:41:46 -0300

    :sparkles: feat: created list users route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import { CreateUserController } from '../controllers/users/CreateUserController'
+import { ListUserController } from '../controllers/users/ListUserController'
 
 const createUserController = new CreateUserController()
+const listUserController = new ListUserController()
+
 
 const router = Router()
 
+router.get('/', listUserController.handle)
 router.post('/', createUserController.handle)
 
 export default router
