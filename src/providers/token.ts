570929a5d5311b2bcd676de95a4b9a4e996817e3
Rafael Angonese <rafael.angonese09@gmail.com>
2022-02-28 22:30:57 -0300

    :sparkles: chore: changed sign token to use public and private key

--- src/providers/token.ts
+++ src/providers/token.ts
@@ -1,17 +1,25 @@
-import jwt from 'jsonwebtoken'
+import jwt, { SignOptions } from 'jsonwebtoken'
 
 interface IPayloadProps {
     id: string;
     role: string;
 }
 
+const signOptions: SignOptions = {
+    algorithm: 'RS256',
+    expiresIn: '7d',
+}
+
+const publicKey = process.env.JWT_PUBLIC_KEY || ""
+const privateKey = process.env.JWT_PRIVATE_KEY || ""
+
 export async function sign(payload: IPayloadProps): Promise<string> {
 
-    const token = jwt.sign(payload, 'secret')
+    const token = jwt.sign(payload, privateKey, signOptions)
 
     return token
 }
 
 export function verify(token: string) {
-    jwt.verify(token, 'secret')
+    jwt.verify(token, publicKey)
 }
