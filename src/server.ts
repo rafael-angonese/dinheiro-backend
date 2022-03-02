e4fb2c4be90f76c574ad9be17c374401522b444f
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-02 18:44:32 -0300

    :heavy_plus_sign: feat: install cookie-parser dependency

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import 'dotenv'
+import cookieParser from 'cookie-parser'
 
 import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
