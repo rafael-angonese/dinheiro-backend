ff0d1d69b35dd47aa918b961e9cd2fcc18865494
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-01 23:21:38 -0300

    :sparkles: feat: created CreateRefreshTokenService

---
+++ src/services/auth/CreateRefreshTokenService.ts
@@ -0,0 +1,25 @@
+import { RefreshToken } from "@prisma/client";
+import authConfig from "../../config/auth.config";
+import { prismaClient } from "../../database/prismaClient";
+import { generateRefreshToken } from "../../providers/token";
+
+export class CreateRefreshTokenService {
+    async execute(userId: string): Promise<Error | RefreshToken> {
+
+        const token = generateRefreshToken(userId)
+
+        const expiresAt = new Date(Date.now() + authConfig.refreshToken.duration)
+
+        const refreshToken = await prismaClient.refreshToken.create({
+            data: {
+                userId: userId,
+                token: token,
+                expiresAt,
+                valid: true
+            }
+        })
+
+        return refreshToken;
+
+    }
+}
