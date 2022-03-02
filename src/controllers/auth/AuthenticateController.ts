8573b4f27ae3f19c0878c3038ff8c44d9473ae3e
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-02 18:46:07 -0300

    :recycle: refactor: set refreshToken into cookies

--- src/controllers/auth/AuthenticateController.ts
+++ src/controllers/auth/AuthenticateController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import authConfig from "../../config/auth.config";
 import { AuthenticateService } from "../../services/auth/AuthenticateService";
 
 const authenticateService = new AuthenticateService();
@@ -9,12 +10,19 @@ export class AuthenticateController {
 
         const result = await authenticateService.execute({ email, password });
 
-        if(result instanceof Error) {
-            return response.status(400).json(result.message); 
+        if (result instanceof Error) {
+            return response.status(400).json(result.message);
         }
 
         const { token, refreshToken } = result
 
-        return response.json({ token, refreshToken })
+        const cookieOptions = {
+            maxAge: authConfig.refreshToken.duration,
+            httpOnly: true,
+        }
+
+        response.cookie('refreshToken', refreshToken, cookieOptions)
+
+        return response.json({ token })
     }
 }
