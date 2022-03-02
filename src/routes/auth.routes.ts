473aa8cb6544559a615262b9921b351373050552
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-02 19:31:03 -0300

    :sparkles: feat: created logout route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express'
 import { AuthenticateController } from '../controllers/auth/AuthenticateController'
+import { LogoutController } from '../controllers/auth/LogoutController'
 import { RefreshTokenController } from '../controllers/auth/RefreshTokenController'
 
 const authenticateController = new AuthenticateController()
 const refreshTokenController = new RefreshTokenController()
+const logoutController = new LogoutController()
 
 
 const router = Router()
 
 router.post('/', authenticateController.handle)
 router.post('/refreshToken', refreshTokenController.handle)
+router.post('/logout', logoutController.handle)
 
 
 export default router
