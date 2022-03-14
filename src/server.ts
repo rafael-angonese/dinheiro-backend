08e5a0ac95301f4d1301d3fbcd0584368ce3b1b1
Rafael Angonese <rafael.angonese09@gmail.com>
2022-03-13 23:31:58 -0300

    :sparkles: feat: configure handling errors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv'
+import 'express-async-errors'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import AppError from './errors/AppError';
 
 import routes from './routes'
 
@@ -16,6 +18,19 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(corsOptions))
 app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json(err);
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+    // message: `Internal server error - ${err.message}`,
+  });
+});
+
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('Server listening on port: ' + process.env.PORT)
