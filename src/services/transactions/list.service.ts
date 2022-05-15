d4cfc4444d9de5447ab5b7b9bc0b7e31743b6ec1
Rafael Angonese <rafael.angonese09@gmail.com>
2022-05-14 22:11:18 -0300

    :sparkles: feat: add query params filters into transactions list

--- src/services/transactions/list.service.ts
+++ src/services/transactions/list.service.ts
@@ -2,15 +2,23 @@ import { Transaction } from "@prisma/client";
 import { prismaClient } from "../../database/prismaClient";
 
 interface IRquestProps {
-    user_id: string
+    user_id: string;
+    account_id: string;
+    startDate: Date;
+    endDate: Date;
 }
 
 export class ListTransactionService {
-    async execute({ user_id }: IRquestProps): Promise<Transaction[]> {
+    async execute(params: IRquestProps): Promise<Transaction[]> {
 
         const data = await prismaClient.transaction.findMany({
             where: {
-                user_id
+                user_id: params.user_id,
+                account_id: params.account_id,
+                date: {
+                    gte: params.startDate,
+                    lte: params.endDate,
+                }
             },
             include: {
                 category: {
