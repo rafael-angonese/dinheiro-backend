95831ad61ea18a5ddf713226967a96032eb66466
Rafael Angonese <rafael.angonese09@gmail.com>
2022-05-21 07:08:42 -0300

    :sparkles: feat: created CreateFileService

---
+++ src/services/files/create.service.ts
@@ -0,0 +1,25 @@
+import { prismaClient } from "../../database/prismaClient";
+import { File } from "../../../prisma/client";
+
+interface IRequest {
+    key: string;
+    model: string;
+    name: string;
+    bucket: string;
+    url: string;
+    size: number;
+    content_type: string;
+};
+
+export class CreateFileService {
+    async execute(params: IRequest): Promise<File> {
+
+        const data = await prismaClient.file.create({
+            data: {
+                ...params,
+            }
+        })
+
+        return data;
+    }
+}
