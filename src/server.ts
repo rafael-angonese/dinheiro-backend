7f97c531701291be7e0118e8ef5763626815ffa3
Rafael Angonese <rafael.angonese09@gmail.com>
2022-05-28 22:58:25 -0300

    :sparkles: feat: included files on list files transactions

--- src/server.ts
+++ src/server.ts
@@ -27,8 +27,8 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
 
   return response.status(500).json({
     status: 'error',
-    message: 'Internal server error',
-    // message: `Internal server error - ${err.message}`,
+    // message: 'Internal server error',
+    message: `Internal server error - ${err.message}`,
   });
 });
 
--- src/services/transactions/list.service.ts
+++ src/services/transactions/list.service.ts
@@ -2,40 +2,52 @@ import { Transaction } from "@prisma/client";
 import { prismaClient } from "../../database/prismaClient";
 
 interface IRquestProps {
-    user_id: string;
-    account_id: string;
-    startDate: Date;
-    endDate: Date;
+  user_id: string;
+  account_id: string;
+  startDate: Date;
+  endDate: Date;
 }
 
 export class ListTransactionService {
-    async execute(params: IRquestProps): Promise<Transaction[]> {
-
-        const data = await prismaClient.transaction.findMany({
-            where: {
-                user_id: params.user_id,
-                account_id: params.account_id,
-                date: {
-                    gte: params.startDate,
-                    lte: params.endDate,
-                }
+  async execute(params: IRquestProps): Promise<Transaction[]> {
+    const data = await prismaClient.transaction.findMany({
+      where: {
+        user_id: params.user_id,
+        account_id: params.account_id,
+        date: {
+          gte: params.startDate,
+          lte: params.endDate,
+        },
+      },
+      include: {
+        category: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        bankAccount: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        fileOnTransaction: {
+          select: {
+            id: true,
+            file_id: true,
+            file: {
+              select: {
+                id: true,
+                content_type: true,
+                name: true,
+              },
             },
-            include: {
-                category: {
-                    select: {
-                        id: true,
-                        name: true
-                    }
-                },
-                bankAccount: {
-                    select: {
-                        id: true,
-                        name: true
-                    }
-                }
-            }
-        })
+          },
+        },
+      },
+    });
 
-        return data;
-    }
+    return data;
+  }
 }
