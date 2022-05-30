9ab9573443a556cf133b3eb9133159d25b16de7d
Rafael Angonese <rafael.angonese09@gmail.com>
2022-05-29 23:15:03 -0300

    :bug: fix: prisma output

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "../prisma"
+import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient()
+const prismaClient = new PrismaClient();
 
-export { prismaClient }
+export { prismaClient };
