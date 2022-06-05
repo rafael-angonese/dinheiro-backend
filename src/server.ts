d7a1c52c3b0f70af654e23e39174a0e8950de200
Rafael Angonese <rafael.angonese09@gmail.com>
2022-06-05 19:37:15 -0300

    :bug: fix: cors options

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 app.use(cors())
 app.use(routes)
 
