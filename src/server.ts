53c1fed75e5ff3846e9435ff60ed8249a76ddd06
Rafael Angonese <rafael.angonese09@gmail.com>
2022-06-05 18:46:01 -0300

    :rocket: build(server): enabled cors

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
 app.use(routes)
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
