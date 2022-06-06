45a4f0de366090d2f01a033d67a2f03d87bf1151
Rafael Angonese <rafael.angonese09@gmail.com>
2022-06-05 21:37:34 -0300

    :bug: fix: refresh token cookie

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors())
-app.use(cors())
 app.use(routes)
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
